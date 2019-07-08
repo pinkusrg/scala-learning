10304206a0b4a7ff4d73dc641b2f6cc7cac74dc6
Divyanshu Srivastava <52560790+Divyanshu564@users.noreply.github.com>
2019-07-08 17:13:44 +0530

    Update Recursion.scala

--- src/main/scala/Function/Recursion.scala
+++ src/main/scala/Function/Recursion.scala
@@ -8,10 +8,5 @@ object Recursion {
     else
       n * factorial(n-1)
   }
-  
-  def recursiveSum(x : Int*) : Int ={
-    if (x.length == 0) 0
-    else x.head + recursiveSum(x.tail : _*)
-  }
 
 }
